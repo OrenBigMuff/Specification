3a255124f947d7235c3829e2cb8c02d61e1f75ff
Dan_Imai <oinarhythm0508@gmail.com>
2016-01-21 10:00:46 +0900

    ブランチのテスト

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,44 +1,47 @@
 /*
-* ƒvƒƒOƒ‰ƒ€–¼:HelloWorld
-* ‹@”\:ˆ¥A
-* ì¬Ò:‰Y“‡
-* ì¬“ú:2015/10/09
-* ƒo[ƒWƒ‡ƒ“:1.0
-* C³—š—ğ:
+* ï¿½vï¿½ï¿½ï¿½Oï¿½ï¿½ï¿½ï¿½ï¿½ï¿½:HelloWorld
+* ï¿½@ï¿½\:ï¿½ï¿½ï¿½A
+* ï¿½ì¬ï¿½ï¿½:ï¿½Yï¿½ï¿½
+* ï¿½ì¬ï¿½ï¿½:2015/10/09
+* ï¿½oï¿½[ï¿½Wï¿½ï¿½ï¿½ï¿½:1.0
+* ï¿½Cï¿½ï¿½ï¿½ï¿½ï¿½ï¿½:
 */
+
+
+//ãŠå‰ã®ã‚³ãƒ¼ãƒ‰ã‚’æ±šã—ã¦ã‚„ã‚‹ï¼ï¼ï¼ï¼ï¼
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		//ˆ¥A‚ğo—Í‚µ‚Ü‚·
+		//ï¿½ï¿½ï¿½Aï¿½ï¿½ï¿½oï¿½Í‚ï¿½ï¿½Ü‚ï¿½
 		/*
 		System.out.println("HelloWorld!!!!");
-		System.out.println("‚±‚ñ‚É‚¿‚íA‰Y“‡‚Å‚·");	
+		System.out.println("ï¿½ï¿½ï¿½ï¿½ï¿½É‚ï¿½ï¿½ï¿½ï¿½Aï¿½Yï¿½ï¿½ï¿½Å‚ï¿½");
 		*/
-		
+
 		/*
-		int age;	//”N—î‚ğ“ü‚ê‚é•Ï”
-		
-		age=20;		//age‚É20‚ğ‘ã“ü‚·‚é
-		
-		//Œ‹‰Ê‚ğo—Í‚·‚é
-		System.out.println("„‚Í" + age + "Î‚Å‚·B");
-		
+		int age;	//ï¿½Nï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½Ïï¿½
+
+		age=20;		//ageï¿½ï¿½20ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½
+
+		//ï¿½ï¿½ï¿½Ê‚ï¿½ï¿½oï¿½Í‚ï¿½ï¿½ï¿½
+		System.out.println("ï¿½ï¿½ï¿½ï¿½" + age + "ï¿½Î‚Å‚ï¿½ï¿½B");
+
 		System.out.println("$" + 20 + 30);
 		System.out.println(20 + 30 + "$");
 		*/
-		
+
 		int year = 2015;
 		int month = 10;
 		int day = 9;
-		
+
 		System.out.print(year + "/" + month + "/" + day );
-		
-		System.out.println("");		//‰üs—p
-		
-		System.out.println(year + "”N" + month + "Œ" + day + "“ú");
-		
-		System.out.printf("¡“ú‚Í%d”N%dŒ%d“ú‚Å‚·",year,month,day);
-		
-		
+
+		System.out.println("");		//ï¿½ï¿½ï¿½sï¿½p
+
+		System.out.println(year + "ï¿½N" + month + "ï¿½ï¿½" + day + "ï¿½ï¿½");
+
+		System.out.printf("ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½%dï¿½N%dï¿½ï¿½%dï¿½ï¿½ï¿½Å‚ï¿½",year,month,day);
+
+
 	}
 }
